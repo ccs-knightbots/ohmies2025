4ac2d126e0a78900b11b8340e7966d7de3f81ed5
RWhite486 <reubenwhite486@gmail.com>
2025-04-14 15:02:20 -0400

    drivetrain implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ManualDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ManualDrive.java
@@ -4,36 +4,36 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class ManualDrive {
-    public DcMotor leftFrontMotor_0;
-    public DcMotor leftBackMotor_1;
-    public DcMotor rightBackMotor_2;
-    public DcMotor rightFrontMotor_3;
+    public DcMotor lf0;
+    public DcMotor lb1;
+    public DcMotor rb2;
+    public DcMotor rf3;
 
     public ManualDrive(HardwareMap hwMap) {
-        leftFrontMotor_0 = hwMap.get(DcMotor.class, "leftFrontMotor_0");
-        leftBackMotor_1 = hwMap.get(DcMotor.class, "leftBackMotor_1");
-        rightBackMotor_2 = hwMap.get(DcMotor.class, "rightBackMotor_2");
-        rightFrontMotor_3 = hwMap.get(DcMotor.class, "rightFrontMotor_3");
+        lf0 = hwMap.get(DcMotor.class, "lf0");
+        lb1 = hwMap.get(DcMotor.class, "lb1");
+        rb2 = hwMap.get(DcMotor.class, "rb3");
+        rf3 = hwMap.get(DcMotor.class, "rf3");
         // Maps motors.
 
-        leftFrontMotor_0.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftBackMotor_1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightBackMotor_2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightFrontMotor_3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lf0.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rf3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         // Sets the mode. *See official FTC guide for more options.
 
-        leftFrontMotor_0.setDirection(DcMotor.Direction.FORWARD);
-        leftBackMotor_1.setDirection(DcMotor.Direction.REVERSE);
-        rightBackMotor_2.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontMotor_3.setDirection(DcMotor.Direction.REVERSE);
+        lf0.setDirection(DcMotor.Direction.FORWARD);
+        lb1.setDirection(DcMotor.Direction.REVERSE);
+        rb2.setDirection(DcMotor.Direction.FORWARD);
+        rf3.setDirection(DcMotor.Direction.REVERSE);
 
     }
 
     public void setDCMotorPower(double leftFrontPower, double leftBackPower, double rightFrontPower, double rightBackPower) {
-        leftFrontMotor_0.setPower(leftFrontPower);
-        leftBackMotor_1.setPower(leftBackPower);
-        rightBackMotor_2.setPower(rightBackPower);
-        rightFrontMotor_3.setPower(rightFrontPower);
+        lf0.setPower(leftFrontPower);
+        lb1.setPower(leftBackPower);
+        rb2.setPower(rightBackPower);
+        rf3.setPower(rightFrontPower);
     }
 
     public void controllerDrive(double axial, double lateral, double yaw) {
